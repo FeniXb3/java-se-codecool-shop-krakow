5a5debed62d466e8d4aba7d09458a1f727d8d46b
FeniXb3 <fenix.b3@gmail.com>
2017-04-27 19:07:36 +0200

    Create shop application class as the entrypoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import com.codecool.shop.CodecoolShopApplication;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        CodecoolShopApplication app = new CodecoolShopApplication();
+        app.start();
     }
 
 
---
+++ src/main/java/com/codecool/shop/CodecoolShopApplication.java
@@ -0,0 +1,8 @@
+package com.codecool.shop;
+
+public class CodecoolShopApplication {
+
+    public void start() {
+        System.out.println("Starting!");
+    }
+}
